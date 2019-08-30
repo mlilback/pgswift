e893447a2ca7f594b1dcfd0d9a962c21fe71da72
Mark Lilback <mark@lilback.com>
2019-08-29 23:59:38 -0400

    added test for executeBinary

--- Tests/pgswiftTests/QueryTests.swift
+++ Tests/pgswiftTests/QueryTests.swift
@@ -9,11 +9,33 @@ import Foundation
 import XCTest
 @testable import pgswift
 
+extension Date {
+	var onlyDate: Date {
+		let calendar = Calendar.current
+		var dateComps = calendar.dateComponents([.year,.month,.day], from: self)
+		dateComps.timeZone = TimeZone.init(abbreviation: "UTC")!
+		return calendar.date(from: dateComps)!
+		
+	}
+}
+
 final class QueryTests: XCTestCase {
 	var connection: Connection?
+	var dateFormatter: DateFormatter?
+	var timestampFormatter = DateFormatter()
 	
 	override func setUp() {
 		let basicInfo = ConnectInfo(host: "localhost", port: "5433", user: "test", password: "secret", dbname: "test", sslMode: .allow)
+		dateFormatter = DateFormatter()
+		dateFormatter?.locale = Locale(identifier: "en_US_POSIX")
+//		dateFormatter?.dateStyle = .short
+//		dateFormatter?.timeStyle = .none
+		dateFormatter?.setLocalizedDateFormatFromTemplate("M/d/yyyy")
+//		dateFormatter?.timeZone = TimeZone.current
+		timestampFormatter.dateFormat = " yyyy-MM-dd HH:m:s.SSSSx"
+		timestampFormatter.locale = Locale(identifier: "en_US_POSIX")
+		timestampFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+
 		connection = Connection(connectInfo: basicInfo)
 		try! connection?.open()
 		XCTAssert(connection?.isConnected ?? false)
@@ -24,7 +46,7 @@ final class QueryTests: XCTestCase {
 				age int,
 				signupDate date DEFAULT NOW(),
 				signupStamp timestamp with time zone default now());
-			INSERT INTO person (id, name, age, signupDate) VALUES (1, 'mark', 46, '2019-01-08');
+			INSERT INTO person (id, name, age, signupDate, signupStamp) VALUES (1, 'mark', 46, '2019-01-08', '2019-08-30 03:13:15.607487+00');
 			INSERT INTO person (id, name, age, signupDate) VALUES (2, 'kenny', 44, '2019-03-11');
 			INSERT INTO person (id, name) VALUES (3, 'brinley');
 		""")
@@ -46,7 +68,7 @@ final class QueryTests: XCTestCase {
 			XCTAssertEqual(try result.getStringValue(row: 0, column: 1), "mark")
 			XCTAssertEqual(try result.getIntValue(row: 1, column: 2), 44)
 			XCTAssertNil(try result.getIntValue(row: 2, column: 2))
-			let signup = try result.getDateValue(row: 0, column: 4)
+			let signup = try result.getDateValue(row: 0, column: 4)!
 			print("signup = \(String(describing: signup))")
 		} catch let err as PostgreSQLError {
 			print(err.localizedDescription)
@@ -55,8 +77,34 @@ final class QueryTests: XCTestCase {
 			XCTFail("unknown error")
 		}
 	}
+
+	func testBinaryQuery() {
+		guard let con = connection else { XCTFail(); return }
+		XCTAssert(con.isConnected)
+		do {
+			let result = try con.executeBinary(query: "select id, name, age, signupDate, signupStamp from person")
+			XCTAssertEqual(result.rowCount, 3)
+			XCTAssertEqual(try result.getStringValue(row: 0, column: 1), "mark")
+			XCTAssertEqual(try result.getIntValue(row: 1, column: 2), 44)
+			XCTAssertNil(try result.getIntValue(row: 2, column: 2))
+			let sdate = try result.getDateValue(row: 0, column: 3)
+			XCTAssertNotNil(sdate)
+			let onlyDate = sdate!.addingTimeInterval(12.0 * 60.0 * 60.0)
+			XCTAssertEqual(dateFormatter!.string(from: onlyDate), "1/8/2019")
+			// not sure how to test timestamp compared to string. dateFormatter with proper format string keep returning nil
+//			let signup = try result.getDateValue(row: 0, column: 4)!
+//			let signupStr = timestampFormatter.string(from: signup)
+//			XCTAssertEqual(signupStr, "2019-08-30 03:13:15.607487+00")
+//			print("signup = \(signupStr)")
+		} catch let err as PostgreSQLError {
+			XCTFail(err.localizedDescription)
+		} catch {
+			XCTFail("unknown error")
+		}
+	}
 	
 	static var allTests = [
 		("testBasicQuery", testBasicQuery),
+		("testBinaryQuery", testBinaryQuery)
 	]
 }
