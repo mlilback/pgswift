ff7e704982c4c633e8a31c5e93c4b715afc4a47f
Mark Lilback <mark@lilback.com>
2019-09-04 13:58:04 -0400

    all instances check to make sure docker container is running

--- Tests/pgswiftTests/BaseTest.swift
+++ Tests/pgswiftTests/BaseTest.swift
@@ -21,6 +21,11 @@ class BaseTest: XCTestCase {
 	let testDb = "test"
 	
 	override func setUp() {
+		guard confirmDockerRunning() else {
+			logger.critical("docker container not running")
+			fatalError("pgtest docker container not running. See README.md.")
+		}
+		
 		let basicInfo = ConnectInfo(host: testHost, port: testPort, user: testUser, password: testPassword, dbname: testDb, sslMode: .allow)
 		dateFormatter = DateFormatter()
 		dateFormatter?.locale = Locale(identifier: "en_US_POSIX")
@@ -41,6 +46,30 @@ class BaseTest: XCTestCase {
 			try! connection?.execute(query: sql)
 		}
 		connection?.close()
+	}
+	
+	func whichDocker() -> URL {
+		let envPath = ProcessInfo.processInfo.environment["DOCKER_EXE"] ?? "/usr/local/bin/docker"
+		return URL(fileURLWithPath: envPath)
+	}
+	
+	func confirmDockerRunning() -> Bool {
+		let proc = Process()
+		proc.executableURL = whichDocker()
+		proc.arguments = ["ps", "--format", "'{{.Names}}'", "--filter", "name=pgtest"]
+		let outPipe = Pipe()
+		proc.standardOutput = outPipe
+		
+		do {
+			try proc.run()
+			proc.waitUntilExit()
+			let outputData = outPipe.fileHandleForReading.readDataToEndOfFile()
+			let outputStr = String(decoding: outputData, as: UTF8.self)
+			return outputStr.contains("pgtest")
+		} catch {
+			logger.critical("failed to run docker ps")
+			return false
+		}
 	}
 	
 	var initialSQL: String { return "" }
